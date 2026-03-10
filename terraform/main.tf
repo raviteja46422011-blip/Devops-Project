a85f25b9e374d2ab6e97d13da02d6e3be20f47e1
raviteja46422011-blip <raviteja46422011@gmail.com>
2026-03-10 18:17:54 +0530

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "azurerm"{
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name 		= "devops-rg"
+  location	= "Central India"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name			= "devops-plan"
+  location		= azurerm_resource_group.rg.location
+  resource_group_name	= azurerm_resource_group.rg.name
+  os_type		= "Windows"
+  sku_name		= "F1"
+}
+
+resource "azurerm_windows_web_app" "webapp" {
+  name			= "devopswebapp1998"
+  location		=azurerm_resource_group.rg.location
+  resource_group_name	=azurerm_resource_group.rg.name
+  service_plan_id	=azurerm_service_plan.plan.id
+
+
+  site_config {}
+}
